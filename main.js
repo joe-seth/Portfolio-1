2c4da5a1db14aedb6bb269823d95147ad1b3c816
joe-seth <mrjoemaina01@gmail.com>
2024-02-15 15:25:19 +0300

    adding Javascript ' '

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed= new Typed(".text", {
+    strings:["Fullstack Developer", "Data Scientist", "Machine Learning Engineer"],
+    typespeed: 100,
+    backspeed: 100,
+    backDelay: 1000,
+    loop: true
+});
